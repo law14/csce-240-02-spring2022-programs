2cdc116bee35951e7d978e394218657028054a35
law14 <law14@email.sc.edu>
2022-04-11 23:16:58 -0400

    BufferedWriterBase created

--- prog_assignments/prog5-sessionlogger/src/CSVFileWriter.java
+++ prog_assignments/prog5-sessionlogger/src/CSVFileWriter.java
@@ -1,18 +1,25 @@
 import java.io.*;
 
 /**
- * The CSVFileAdjuster class follows the singleton design pattern.
+ * The CSVFileAdjuster class follows the singleton design pattern
+ *  * and extends the abstract BufferedWriterBase class.
  * To use first "build()" then "writeToFile()" then "end()"
  * @author Luke
  */
 public class CSVFileWriter extends BufferedWriterBase {
     private static CSVFileWriter fileAdjuster = new CSVFileWriter();
     BufferedWriter writer;
-    // private File outputFile;
-    // private boolean built = false;
 
+    /**
+     * Private CSVFileWriter constructor since this will follow
+     * the singleton design pattern.
+     */
     private CSVFileWriter() {}
 
+    /**
+     * Use to get the single CSVFileWriter instance
+     * @return the single CSVFileWriter instance
+     */
     public static CSVFileWriter getInstance() {
         if (fileAdjuster != null) {
             fileAdjuster = new CSVFileWriter();
@@ -20,5 +27,7 @@ public class CSVFileWriter extends BufferedWriterBase {
         return fileAdjuster;
     }
     
+    public void 
+
 
 }
